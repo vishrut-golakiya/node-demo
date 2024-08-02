8f32b822a190af7643be01c0c3de7208ad615b83
vishrut <vishrutgolakiya@gmail.com>
2024-08-02 11:42:10 +0530

    fo

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('Hello! this is demo for node.js app');
 });
 
 app.listen(port, () => {
